abf49bf4f9773e5bdc2799b4df462a49273874cb
Xerolynx <xerolynx06@gmail.com>
2025-12-23 18:48:36 +0530

    Initial commit: OpenGL window with color buffer

---
+++ README.md
@@ -0,0 +1,23 @@
+# Basic OpenGL Window & Colour Buffer
+ Basic **Opengl** Code with Custom Window Colour
+
+ ##PROCESS
+ -- By using **GLAD** Header Library
+ -- By Adding Values in glClearColor();
+ -- glClearColor(R, G, B, A);
+
+ ##Some Info
+ -- We can Add Many Colours
+ -- R = Red , G = Green , B = Blue , A = Alpha (Opacity)
+ -- For Blue = (0.0f, 0.0f, 1.0f, 1.0f)
+ -- For Red = (1.0f, 0.0f, 0.0f, 1.0f)
+ -- For Green = (0.0f, 1.0f, 0.0f, 1.0f)
+ -- For White = (1.0f, 1.0f, 1.0f, 1.0f)
+ -- For Black = (0.0f, 0.0f, 0.0f, 1.0f)
+ -- Just Substitute the float points in glClearColor
+ -- There Are Many Colors , These Are Primary 
+
+ ##Tools Used
+ -- **OpenGL(Graphic API)**
+ -- IDE - **Visual Studio**
+ -- Language - **C++**
---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	if (!glfwInit()) {
+
+		cout << "Failed to Initialize";
+
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindow* window = glfwCreateWindow(800, 800, "OpenGL Window",NULL,NULL); 
+
+	if (window == NULL) {
+
+		cout << "Window Creation Failed";
+
+		glfwTerminate();
+
+		return -1;
+
+		}
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadProc)glfwGetProcAddress)) {
+
+		cout << "GLAD Initialization Failed";
+
+		return -1;
+
+	}
+	glViewport(0, 0, 800, 800);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);//Blue Colour
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+
+}
